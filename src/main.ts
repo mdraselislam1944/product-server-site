e1e9193e86f288e1d625885595b57016263880c7
Md Rasel Islam <mdraselislam1944@gmail.com>
2024-11-08 14:33:18 +0600

    feat: swagger add

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('port is calling now: ', process.env.PORT);
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Products documentation')
+    .setDescription('The products API description')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
